7841963604f09d7e2f8deb748bf6106a03c262e4
Dilan Jayamanne <dicmandilan@gmail.com>
2021-01-28 00:02:41 +0530

    quick union added

--- src/DynamicConnectivity.java
+++ src/DynamicConnectivity.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
-
-public class DynamicConnectivity {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        UF uf = new UF(N
-    }
-}
+//import java.util.Scanner;
+//
+//public class DynamicConnectivity {
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//        UF uf = new UF(N
+//    }
+//}
